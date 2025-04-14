6b44b38ebf343bdd957d41e6c473f0c2c5c54fb3
LawLab Soft Ltd. <itslatifur@gmail.com>
2025-04-14 21:36:18 +0600

    DAY 1

---
+++ 021 - if.js
@@ -0,0 +1,23 @@
+/****** Examples of If ********/
+
+// Comparing two numbers a and b
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log("a is greater than b"); // Executes if a is greater than b
+}
+if (a < b) {
+  console.log("a is less than b"); // Executes if a is less than b
+}
+
+// Checking if a number n is even or odd
+var n = 50;
+
+if (n % 2 == 0) {
+  console.log(n + " is even"); // Executes if n is divisible by 2
+}
+
+if (n % 2 == 1) {
+  console.log(n + " is odd"); // Executes if n is not divisible by 2
+}
---
+++ 022 - else.js
@@ -0,0 +1,23 @@
+/****** Examplea of Else ********/
+
+
+// Comparing two numbers a and b
+var a = 10;
+var b = 20;
+
+if (a > b) {
+  console.log("a is greater than b"); // Executes if a is greater than b
+} else if (a < b) {
+  console.log("a is less than b"); // Executes if a is less than b
+} else {
+  console.log("a is equal to b"); // Executes if a is equal to b
+}
+
+// Checking if a number n is even or odd
+var n = 50;
+
+if (n % 2 == 0) {
+  console.log(n + " is even"); // Executes if n is divisible by 2
+} else {
+  console.log(n + " is odd"); // Executes if n is not divisible by 2
+}

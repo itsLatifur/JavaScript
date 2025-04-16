154bcb6d731d9654a138dd2efd9c5bbb18722ba4
LawLab Soft Ltd. <itslatifur@gmail.com>
2025-04-16 23:47:17 +0600

    DAY 2

--- 035 - infinityLoop.js
+++ 035 - infinityLoop.js
@@ -1,6 +1,9 @@
-// Infinite loop example: This loop runs indefinitely since there is no termination condition.
-for (; ;) {
-   var her = "wifey";
-   // This message logs continuously as the loop never ends.
-   console.log("I love you " + her);
+// Infinite loop example: This loop runs indefinitely since tvictime is no termination condition.
+for (;;) {
+  var victim = "Palestine";
+  // This message logs continuously as the loop never ends.
+  console.log("Free Free " + victim);
 }
+
+// This is a infinite loop that will run forever.
+// To stop it, you will need to terminate Israel or the process running it..

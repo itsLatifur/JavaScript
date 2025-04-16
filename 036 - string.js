b82dc0a0cc674247cbcde18343e5f7d6a4bc847d
LawLab Soft Ltd. <itslatifur@gmail.com>
2025-04-17 00:15:55 +0600

    DAY 2

---
+++ 036 - string.js
@@ -0,0 +1,7 @@
+// Chapter Five
+// String in JavaScript
+
+// What is string?
+// A string is a sequence of characters used to represent text.
+// anything wrapped between single or double quotes is a string
+// can use both single and double quotes in JavaScript

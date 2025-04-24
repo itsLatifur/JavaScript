e6bae525dec7af0a61a697b25cba058b43f4f6bb
LawLab Soft Ltd. <itslatifur@gmail.com>
2025-04-24 23:34:14 +0600

    DAY 7

--- 066 - functionScoping.js
+++ 066 - functionScoping.js
@@ -1,3 +1,15 @@
-// function Scoping examples in JavaScript
+// Function Scoping examples in JavaScript
 
-var a = 'abc'
+var a = 'abc' // Global scope variable
+
+function x() {
+    var a = 20 // Local scope variable inside function x
+    function y() {
+        var a = 10 // Local scope variable inside function y
+        console.log(a) // Output: 10 (y's local variable takes precedence)
+    }
+    console.log(a) // Output: 20 (x's local variable takes precedence over the global variable)
+    y() // Call function y
+}
+
+x() // Call function x

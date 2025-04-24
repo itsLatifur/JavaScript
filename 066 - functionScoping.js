9d3d57f17d3af2844a7a88c49c49b7f5c3e6d16a
LawLab Soft Ltd. <itslatifur@gmail.com>
2025-04-24 23:39:51 +0600

    DAY 7

--- 066 - functionScoping.js
+++ 066 - functionScoping.js
@@ -13,3 +13,22 @@ function x() {
 }
 
 x() // Call function x
+
+
+function test(n) {
+    function a() {
+        return n % 3 == 0 // Check if n is divisible by 3
+    }
+    function b() {
+        return n % 5 == 0 // Check if n is divisible by 5
+    }
+
+    if (a() && b()) { // Check if n is divisible by both 3 and 5
+        console.log(n + ' : divisible by both') // Output if divisible by both
+    } else {
+        console.log(n + ' : not divisible by both') // Output if not divisible by both
+    }
+}
+
+test(15) // Call test with 15 (divisible by both 3 and 5)
+test(10) // Call test with 10 (not divisible by both 3 and 5)
